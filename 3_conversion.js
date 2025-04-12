0bad58bb1e6eb9bf3ca5e99defbef93637b8c1cc
moumita-1-2-3 <118422726+moumita-1-2-3@users.noreply.github.com>
2025-04-12 13:42:49 +0530

    First commit with JS files

---
+++ 3_conversion.js
@@ -0,0 +1,7 @@
+let isLoggedin = "momo"
+let booleanisloggedin = Boolean(isLoggedin)
+// console.log(booleanisloggedin)
+
+let sumno = 33
+let stringno= String(sumno)
+console.log(typeof stringno)
---
+++ first.js
@@ -0,0 +1,22 @@
+// let nam="Moumita Patra";
+// console.log(nam);
+// let a =6.2
+// let b ="momo"
+// b = b+5
+// console.log(a)
+// console.log(b)
+// {
+//     let b =5
+//     console.log(b)
+
+// }
+// console.log(b)
+// console.log(typeof a )
+let student ={
+    age :15,
+     name:"momo",
+     sub: "cse", 
+    cgpa : 7.5 ,
+    semester:8
+}
+console.log(  typeof student.age)
